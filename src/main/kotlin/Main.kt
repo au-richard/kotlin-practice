19f4a239879c32f53a9ad7729ef9ddc0d24c4cba
Richard Au <au.richard4@gmail.com>
2023-04-24 17:29:29 -0700

    Testing null check.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,7 @@
 val name = "Richard"
 var greeting: String? = null
-fun main(args: Array<String>) {
-//    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//    println("Program arguments: ${args.joinToString()}
-    when (greeting) {
-        null -> println("Hi")
-        else -> println(greeting)
-    }
-    println(greeting)
+fun main() {
+    val greetingToPrint = if (greeting != null) greeting else "Hi"
+    println(greetingToPrint)
     println(name)
-
 }
