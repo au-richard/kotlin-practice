6c61e3a7fe363b3e27e92337f7d2b529f893633f
Richard Au <au.richard4@gmail.com>
2023-04-25 11:26:54 -0700

    Testing named parameter default values.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,13 +27,14 @@ val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
 //  map.put(4, "d")
 //  map.forEach { (key, value) -> println("$key -> $value") }
 
-fun main() {
-  fun sayHello(greeting: String, vararg itemsToGreet: String) {
-    itemsToGreet.forEach { itemToGreet ->
-      println("$greeting $itemToGreet")
-    }
+fun sayHello(greeting: String, vararg itemsToGreet: String) {
+  itemsToGreet.forEach { itemToGreet ->
+    println("$greeting $itemToGreet")
   }
+}
 
-  val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-  sayHello(greeting = "Hi", *interestingThings)
+fun greetPerson(greeting: String = "Hello", name: String = "Kotlin") = println("$greeting $name")
+
+fun main() {
+  greetPerson(name = "nate")
 }
