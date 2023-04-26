11273d38e3bc4c4a58702774a07c93c3e72b34dc
Richard Au <au.richard4@gmail.com>
2023-04-26 11:57:56 -0700

    Practicing companion objects.

---
+++ src/main/kotlin/EntityFactory.kt
@@ -0,0 +1,19 @@
+interface IdProvider {
+  fun getId(): String
+}
+
+class Entity private constructor(val id: String) {
+  companion object Factory : IdProvider {
+    override fun getId(): String {
+      return "123"
+    }
+
+    const val id = "id"
+    fun create() = Entity(id)
+  }
+}
+
+fun main () {
+  val entity = Entity.Factory.create()
+  Entity.id
+}
