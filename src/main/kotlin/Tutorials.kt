ae98c44d6ef74fd2e43aaf0ab1e761dca3f708e2
Richard Au <au.richard4@gmail.com>
2023-05-08 16:14:11 -0700

    praciting mutable lists.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -1,11 +1,17 @@
-import java.util.*
-import kotlin.math.pow
-  var sphereRadius = 5.5
-  var sphereVolume = 0.75 * Math.PI * sphereRadius.pow(3.0)
-  val string = "richard"
+//import java.util.*
+//import kotlin.math.pow
+//  val x = 7
+//val y = 7
+//  var sphereRadius = 5.5
+//  var sphereVolume = 0.75 * Math.PI * sphereRadius.pow(3.0)
+//  val string = "richard"
+//  println("The volume of the sphere is $sphereVolume")
+//  println("Our string is: ${string.uppercase().reversed()}")
+//  val numArr = arrayOf(1, 2, 3)
+//  println(numArr.contentToString())
 
 fun main() {
-
-  println("The volume of the sphere is $sphereVolume")
-  println("Our string is: ${string.uppercase().reversed()}")
+  val list = mutableListOf(1, 2, 3)
+  list.remove(2)
+  println(list)
 }
