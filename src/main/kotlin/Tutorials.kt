5b2c588c57834c03c330a21042ebb2c55fa79500
Richard Au <au.richard4@gmail.com>
2023-05-08 15:16:16 -0700

    Calculating sphere volume.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -1,8 +1,8 @@
 import kotlin.math.pow
-
-fun main() {
   var sphereRadius = 5.5
   var sphereVolume = 0.75 * Math.PI * sphereRadius.pow(3.0)
+
+fun main() {
 
   println("The volume of the sphere is $sphereVolume")
 }
