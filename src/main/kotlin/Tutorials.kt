e41bca31110653dc0a3597c31f0aa0dcbffc20a6
Richard Au <au.richard4@gmail.com>
2023-05-08 15:19:46 -0700

    praciting string methods.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -1,8 +1,11 @@
+import java.util.*
 import kotlin.math.pow
   var sphereRadius = 5.5
   var sphereVolume = 0.75 * Math.PI * sphereRadius.pow(3.0)
+  val string = "richard"
 
 fun main() {
 
   println("The volume of the sphere is $sphereVolume")
+  println("Our string is: ${string.uppercase().reversed()}")
 }
