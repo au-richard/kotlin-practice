96b478bd2000141a0a038d85a431ffc6f1cba00d
Richard Au <au.richard4@gmail.com>
2023-05-09 12:45:25 -0700

    Practicing user inputs, handling null values.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -23,29 +23,44 @@
 //  val hardExpression = !(x != z ) && bool || z > (x + y) && (!bool || y < z)
 //  println(hardExpression)
 
+
+//  Practicing Conditions
+//  val x = 8
+//  val y = 7
+//  val z = if (x + y == 14) 3 else 4
+//  if (x < y || y * y == 49) {
+//    println("at least one of the conditions is true")
+//  } else if (x > y) {
+//      println("x is greater than y")
+//  } else {
+//      println("x is equal to y")
+//  }
+//  println("the value of z is $z")
+//  val string = "callers"
+//  if (string.reversed() == string) println("$string is a palindrome") else println("$string is not a palindrome")
+
+//  Practicing Null Values and User Input
+
+
 //  Practicing Arrays and Lists
 //  val numArr = arrayOf(1, 2, 3)
 //  val list = mutableListOf(1, 2, 3)
 //  list.remove(2)
 //  println(list)
-
-//  Practicing Conditions
-
-
-//  val string = "callers"
-//  if (string.reversed() == string) println("$string is a palindrome") else println("$string is not a palindrome")
 fun main() {
-  val x = 8
-  val y = 7
-  val z = if (x + y == 14) 3 else 4
-  if (x < y || y * y == 49) {
-    println("at least one of the conditions is true")
-  } else if (x > y) {
-      println("x is greater than y")
-  } else {
-      println("x is equal to y")
+//  val userInput = readLine()
+//  if (userInput != null) {
+//    println(userInput.toInt() - 5)
+//  }
+  println("Enter your age")
+  val age = readlnOrNull()
+  if (age != null) {
+    if (age.toInt() in 18..65) {
+      println("You are an adult")
+    } else if (age.toInt() > 65){
+      println("You are really old.")
+    } else {
+      println("You are not an adult yet.")
+    }
   }
-  println("the value of z is $z")
-
-
 }
