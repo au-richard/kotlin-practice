8b252412ff51a4aeed650f2229b4820e55b8b1f6
Richard Au <au.richard4@gmail.com>
2023-05-09 11:12:23 -0700

    Practicing comparison operators with else if.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -28,12 +28,24 @@
 //  val list = mutableListOf(1, 2, 3)
 //  list.remove(2)
 //  println(list)
-fun main() {
-  val x = 5
-  val y = 7
-  if (x < y) println("x is less than y")
-  println("this will always be executed")
 
-  val string = "callers"
-  if (string.reversed() == string) println("$string is a palindrome") else println("$string is not a palindrome")
+//  Practicing Conditions
+
+
+//  val string = "callers"
+//  if (string.reversed() == string) println("$string is a palindrome") else println("$string is not a palindrome")
+fun main() {
+  val x = 8
+  val y = 7
+  val z = if (x + y == 14) 3 else 4
+  if (x < y || y * y == 49) {
+    println("at least one of the conditions is true")
+  } else if (x > y) {
+      println("x is greater than y")
+  } else {
+      println("x is equal to y")
+  }
+  println("the value of z is $z")
+
+
 }
