cabc0866c66d336875cbce0e8cdce09137e86767
Richard Au <au.richard4@gmail.com>
2023-05-09 11:07:12 -0700

    Testing inline conditions.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -1,25 +1,39 @@
 //import java.util.*
 //import kotlin.math.pow
+
+// Practicing Arithmetic Operators
 //  val x = 7
-//val y = 7
+//  val y = 7
 //  var sphereRadius = 5.5
 //  var sphereVolume = 0.75 * Math.PI * sphereRadius.pow(3.0)
-//  val string = "richard"
 //  println("The volume of the sphere is $sphereVolume")
-//  println("Our string is: ${string.uppercase().reversed()}")
-//  val numArr = arrayOf(1, 2, 3)
-//  println(numArr.contentToString())
 
+// Practicing Template Literals
+//  val string = "richard"
+//  println("Our string is: ${string.uppercase().reversed()}")
+//  println(numArr.contentToString())
+//  val string = "richard"
+//  println("this is a string: ${string.uppercase().reversed()}")
+
+//  Testing structural vs referential equality
+//  val bool = true
+//  val x = 9
+//  val y = 3
+//  val z = 9
+//  val hardExpression = !(x != z ) && bool || z > (x + y) && (!bool || y < z)
+//  println(hardExpression)
+
+//  Practicing Arrays and Lists
+//  val numArr = arrayOf(1, 2, 3)
+//  val list = mutableListOf(1, 2, 3)
+//  list.remove(2)
+//  println(list)
 fun main() {
-  val list = mutableListOf(1, 2, 3)
-  list.remove(2)
-  println(list)
-  val string = "example string"
-  println("this is a string: ${string.uppercase()}")
-  val bool = true
-  val x = 9
-  val y = 3
-  val z = 9
-  val hardExpression = !(x != z ) && bool || z > (x + y) && (!bool || y < z)
-  println(hardExpression)
+  val x = 5
+  val y = 7
+  if (x < y) println("x is less than y")
+  println("this will always be executed")
+
+  val string = "callers"
+  if (string.reversed() == string) println("$string is a palindrome") else println("$string is not a palindrome")
 }
