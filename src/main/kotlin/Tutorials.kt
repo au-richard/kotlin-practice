815ab2d825a41b1b82681496934575fb270d9b4f
Richard Au <au.richard4@gmail.com>
2023-05-10 13:46:13 -0700

    Reversing user list output.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -133,12 +133,15 @@ import kotlin.math.pow
 
 
 fun main() {
+//  val list = mutableListOf<Int>()
+//  for (i in 1..5) {
+//    val x = readlnOrNull()?.toInt()
+//    if (x != null) {
+//      list.add(x)
+//    }
+//  }
+//  println(list.reversed())
+
   val list = mutableListOf<Int>()
-  for (i in 1..10) {
-    val x = readlnOrNull()?.toInt()
-    if (x != null) {
-      list.add(x)
-    }
-  }
-  println(list)
+
 }
