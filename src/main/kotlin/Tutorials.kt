a835810adb63fcbeb1c082f044c18e617cde2cbe
Richard Au <au.richard4@gmail.com>
2023-05-11 11:22:45 -0700

    Practicing when condition.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -158,14 +158,28 @@ import kotlin.math.pow
 //  Practicing When
 
 fun main() {
-  println("Enter your age")
-  val age = readLine()?.toInt()
-  when(age) {
-    in 0..5 -> println("You're a young kid")
-    in 6..17 -> println("You're a teenager")
-    18 -> println("Finally, you're 18!")
-    19, 20 -> println("You're young adult")
-    in 21..65 -> println("You're an adult")
-    else -> println("You're really old")
+//  println("Enter your age")
+//  val age = readLine()?.toInt()
+//  when(age) {
+//    in 0..5 -> println("You're a young kid")
+//    in 6..17 -> println("You're a teenager")
+//    18 -> println("Finally, you're 18!")
+//    19, 20 -> println("You're young adult")
+//    in 21..65 -> println("You're an adult")
+//    else -> println("You're really old")
+//  }
+
+  println("Where are you from?")
+  when (readlnOrNull()) {
+    "India" -> println("Namaste")
+    "USA" -> println("Hello")
+    "France" -> println("Bonjour")
+    "Germany" -> println("Hallo")
+    "Russia" -> println("Privet")
+    "China" -> println("你好")
+    "Japan" -> println("こんにちは")
+    else -> println("I don't know that")
   }
+
+
 }
