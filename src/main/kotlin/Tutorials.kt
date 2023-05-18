a7e5ad81d308ac0e3a75b68bccf6d4413baca9c6
Richard Au <au.richard4@gmail.com>
2023-05-18 10:47:38 -0700

    Practicing extension functions.

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -271,34 +271,34 @@
 
 
 fun main() {
-  println("Please enter a number")
-  val input = readlnOrNull()?.toInt()
-  if (input != null) {
-    if (input.isPrime()) {
-      println("$input is a prime number")
-    } else {
-      println("$input is not a prime numberj")
-    }
-  }
-//  val num = arrayOf(1, 2, 3, 4, 5, 6)
-//  println("The product of ${num.joinToString(", ")} is ${quotient(num)}")
-}
-
-fun Int.isPrime(): Boolean {
-  for (i in 2 until this - 1) {
-    if (this % i == 0) {
-      return false
-    }
-  }
-  return true
-}
-
-//fun quotient(numArr: Array<Int>): Int {
-//  var result = 1
-//  for (i in numArr) {
-//    result *= i
+//  println("Please enter a number")
+//  val input = readlnOrNull()?.toInt()
+//  if (input != null) {
+//    if (input.isPrime()) {
+//      println("$input is a prime number")
+//    } else {
+//      println("$input is not a prime numberj")
+//    }
 //  }
-//  return result
+  val num = arrayOf(1, 2, 3, 4, 5, 6)
+  println("The product of ${num.joinToString(", ")} is ${quotient(num)}")
+}
+
+//fun Int.isPrime(): Boolean {
+//  for (i in 2 until this - 1) {
+//    if (this % i == 0) {
+//      return false
+//    }
+//  }
+//  return true
 //}
+
+fun quotient(numArr: Array<Int>): Int {
+  var result = 1
+  for (i in numArr) {
+    result *= i
+  }
+  return result
+}
 
 
